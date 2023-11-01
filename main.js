6ae70f0a5610786058612d17003cf319dd9be069
fcarnelose <124386850+fcarnelose@users.noreply.github.com>
2023-11-01 13:49:21 -0300

    0111

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ function tocaSomPom(){
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//01-11-2023- lista numerada
+let contador = 0;
+
+while(contador <listaDeTeclas.length){
+
+    listaDeTeclas[0].onclick = tocaSomPom;
+    contador = contador + 1;
+
+    console.log(contador);
+}
